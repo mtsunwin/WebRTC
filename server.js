c9072c46fa0c4d735739c9c512db84fcd869b66a
Tran Thang <thangtran.se@gmail.com>
2018-11-21 15:15:35 +0700

    thang

--- server.js
+++ server.js
@@ -19,4 +19,12 @@ routeChat(app)
 routeIndex(app)
 routeVideoCall(app)
 
-
+app.route('/thangtm')
+        .get((req, response) => {
+            fs.readFile('./public/thangtm/index.html', function (err, html) {
+                if (err) throw err;
+                response.writeHeader(200, { "Content-Type": "text/html" });
+                response.write(html);
+                response.end();
+            })
+        })
